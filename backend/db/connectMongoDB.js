f74992913f4d41edb44cdbadd153901ba2142f81
AmeyBangde29 <bangdeamey@gmail.com>
2024-04-25 19:56:23 +0530

    1-auth controllers added

--- backend/db/connectMongoDB.js
+++ backend/db/connectMongoDB.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const connectMongoDB = async () => {
     try {
         const dbConnection = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`MongoDB connected: ${dbConnection.connection.host}`)
+        console.log(`MongoDB connected !!`)
     } catch (error) {
         console.log(`Error connection to mongoDB: ${error.message}`)
         process.exit(1)
